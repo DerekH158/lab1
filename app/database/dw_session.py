525caa3314ab1b779b225c5e7539310fa0b6450a
kenneth <kiluore@yahoo.co.uk>
2021-10-30 04:12:37 -0400

    LAB1

---
+++ app/database/dw_session.py
@@ -0,0 +1,16 @@
+import os
+from sqlalchemy import  create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+host = os.environ["HOST"]
+port = os.environ["PORT"]
+user = os.environ["USER"]
+password = os.environ["PASSWORD"]
+db = os.environ["POSTGRESS_DW"]
+dbtype = "postgresql+psycopg2 "
+
+SQLALCHEMY_DATABASE_URL = f"{dbtype}://{user}:{password}@{host}:{port}/{db}"
+
+dwEngine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping = True)
+dwSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=dwEngine)
